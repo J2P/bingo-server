5d563d4ef79052cd219ac8ac61099dd859e392d2
J2P <jjp5023@gmail.com>
2015-05-01 23:47:14 +0900

    Close #1 bingo 게임 데이터를 서버에서 처리 하도록 수정

---
+++ index.js
@@ -0,0 +1,30 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var util = require('./util.js');
+var users = {};
+var colors = ['blue', 'green', 'red', 'orange', 'purple'];
+
+io.on('connection', function(socket){
+  socket.on('send:number', function(data) {
+    for (var user in users) {
+      users[user]['board'] = util.setSelectNumbers(users[user]['board'], data.number);
+    }
+    
+    socket.emit('change:number', { users: users });  
+  	socket.broadcast.emit('change:number', { users: users });
+  });
+
+  socket.on('join', function(data) {
+  	users[data.user] = {
+  		board: util.getRandomNumbers(),
+  		color: colors.splice(Math.floor(Math.random() * colors.length), 1)[0]
+  	};
+
+  	socket.emit('init', users[data.user]);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
