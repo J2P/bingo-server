e48b635b014484a86e794c66a7d4fb7a8028baa0
J2P <jjp5023@gmail.com>
2015-05-05 11:49:41 +0900

    라인별 번호 선택 확인 함수 추가

--- util.js
+++ util.js
@@ -40,7 +40,53 @@ function setSelectNumbers(userBoard, number) {
 	return board
 }
 
+function selectedConfirm(cells) {
+	var confirm = true;
+	cells.forEach(function(cell) {
+		if (!cell.selected) {
+			confirm = false;
+		}
+	});
+
+	return confirm;
+}
+
+function checkLines(userBoard) {
+	var lines = [];
+	var left_cross = [userBoard[0], userBoard[6], userBoard[12], userBoard[18], userBoard[24]];
+	var right_cross = [userBoard[4], userBoard[8], userBoard[12], userBoard[16], userBoard[20]];
+	
+	var horizontal_line_one = [userBoard[0], userBoard[1], userBoard[2], userBoard[3], userBoard[4]];
+	var horizontal_line_two = [userBoard[5], userBoard[6], userBoard[7], userBoard[8], userBoard[9]];
+	var horizontal_line_three = [userBoard[10], userBoard[11], userBoard[12], userBoard[13], userBoard[14]];
+	var horizontal_line_four = [userBoard[15], userBoard[16], userBoard[17], userBoard[18], userBoard[19]];
+	var horizontal_line_five = [userBoard[20], userBoard[21], userBoard[22], userBoard[23], userBoard[24]];
+
+	var vertical_line_one = [userBoard[0], userBoard[5], userBoard[10], userBoard[15], userBoard[20]];
+	var vertical_line_two = [userBoard[1], userBoard[6], userBoard[11], userBoard[16], userBoard[21]];
+	var vertical_line_three = [userBoard[2], userBoard[7], userBoard[12], userBoard[17], userBoard[22]];
+	var vertical_line_four = [userBoard[3], userBoard[8], userBoard[13], userBoard[18], userBoard[23]];
+	var vertical_line_five = [userBoard[4], userBoard[9], userBoard[14], userBoard[19], userBoard[24]];
+
+	
+	if (selectedConfirm(left_cross)) { lines.push('line left_cross'); }
+	if (selectedConfirm(right_cross)) { lines.push('line right_cross'); }
+	if (selectedConfirm(horizontal_line_one)) { lines.push('line horizontal horizontal_line_one'); }
+	if (selectedConfirm(horizontal_line_two)) { lines.push('line horizontal horizontal_line_two'); }
+	if (selectedConfirm(horizontal_line_three)) { lines.push('line horizontal horizontal_line_three'); }
+	if (selectedConfirm(horizontal_line_four)) { lines.push('line horizontal horizontal_line_four'); }
+	if (selectedConfirm(horizontal_line_five)) { lines.push('line horizontal horizontal_line_five'); }
+	if (selectedConfirm(vertical_line_one)) { lines.push('line vertical vertical_line_one'); }
+	if (selectedConfirm(vertical_line_two)) { lines.push('line vertical vertical_line_two'); }
+	if (selectedConfirm(vertical_line_three)) { lines.push('line vertical vertical_line_three'); }
+	if (selectedConfirm(vertical_line_four)) { lines.push('line vertical vertical_line_four'); }
+	if (selectedConfirm(vertical_line_five)) { lines.push('line vertical vertical_line_five'); }
+
+	return lines;
+}
+
 module.exports = {
 	getRandomNumbers: getRandomNumbers,
-	setSelectNumbers: setSelectNumbers
+	setSelectNumbers: setSelectNumbers,
+	checkLines: checkLines
 }
