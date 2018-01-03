2e434d53111af8890baa8b3ea4978fc7279ff33e
J2P <jjp5023@gmail.com>
2018-01-04 02:29:50 +0900

    room 구현

--- index.js
+++ index.js
@@ -1,23 +1,62 @@
-var io = require("socket.io")();
-var uuidv4 = require("uuid/v4");
-var util = require("./lib/util.js");
-var users = {};
+const io = require("socket.io")();
+const util = require("./lib/util.js");
+let users = {};
+let roomNumber = 0;
+
+function roomCount(rooms) {
+  let count = 0;
+  for (room in rooms) {
+    if (room.indexOf("room") === 0) {
+      count++;
+    }
+  }
+
+  return count;
+}
 
 io.on("connection", function(socket) {
+  socket.emit("connected");
+  const rooms = io.sockets.adapter.rooms;
+
+  socket.on("disconnect", function() {
+    socket.emit("disconnect");
+  });
+
   socket.on("join", function(data) {
-    var id = uuidv4();
+    let isJoin = false;
+    let roomName = "";
+    const id = socket.id;
+
+    if (roomCount(rooms) === 0) {
+      roomName = `room${roomNumber}`;
+      socket.join(roomName);
+    } else {
+      for (room in rooms) {
+        if (room.indexOf("room") === 0 && rooms[room].length < 2) {
+          roomName = room;
+          socket.join(roomName);
+          isJoin = true;
+        }
+      }
+
+      if (!isJoin) {
+        roomName = `room${++roomNumber}`;
+        socket.join(roomName);
+      }
+    }
+
     users[id] = {
       id: id,
       board: util.getRandomBoard(),
-      color: util.getRandomColor()
+      color: util.getRandomColor(),
+      roomName: roomName
     };
 
     socket.emit("init", users[id]);
-    socket.broadcast.emit("update:user", { users: users });
   });
 
   socket.on("send:number", function(data) {
-    for (var user in users) {
+    for (user in rooms[data.roomName].sockets) {
       users[user]["board"] = util.setSelectNumbers(
         users[user]["board"],
         data.number
@@ -26,7 +65,7 @@ io.on("connection", function(socket) {
     }
 
     socket.emit("select:number", { users: users });
-    socket.broadcast.emit("select:number", { users: users });
+    io.sockets.in(data.roomName).emit("select:number", { users: users });
   });
 });
 
